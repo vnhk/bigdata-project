1dd602a1d244ed7e8b1f7f14c98861e0ef4be490
vnhk <47853604+vnhk@users.noreply.github.com>
2021-06-07 19:19:19 +0200

    Fix dependencies

--- src/main/java/com/bigdata/navigation/database/ChartRepository.java
+++ src/main/java/com/bigdata/navigation/database/ChartRepository.java
@@ -1,6 +1,8 @@
 package com.bigdata.navigation.database;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ChartRepository extends JpaRepository<Chart, Long> {
 }
